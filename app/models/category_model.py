0ba004a76013bf811566a6dbbda561866121d437
samuel-racak <samuel.racak@gmail.com>
2025-03-17 15:55:13 +0100

    feat: Add Category model for managing product categories

---
+++ app/models/category_model.py
@@ -0,0 +1,9 @@
+from sqlmodel import Field, SQLModel
+
+
+class Category(SQLModel, table=True):
+    __tablename__ = "categories"
+
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(max_length=255)
+    # description: str = Field(max_length=255) TODO: check with the team
