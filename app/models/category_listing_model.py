dea7b626bda9da68a3a9b3c0b780bdd34bde2781
samuel-racak <samuel.racak@gmail.com>
2025-03-17 20:56:13 +0100

    fix: Update CategoryListing model to set category_id and listing_id as primary keys

--- app/models/category_listing_model.py
+++ app/models/category_listing_model.py
@@ -5,5 +5,5 @@ class CategoryListing(SQLModel, table=True):
     __tablename__ = "categoriesListing"
 
     # Foreign keys
-    category_id: int = Field(foreign_key="categories.id")
-    listing_id: int = Field(foreign_key="listings.id")
+    category_id: int = Field(foreign_key="categories.id", primary_key=True)
+    listing_id: int = Field(foreign_key="listings.id", primary_key=True)
