63d828250b734296ee38e37d928f9f2aacf973a4
samuel-racak <samuel.racak@gmail.com>
2025-03-17 20:34:48 +0100

    feat: Import models for Address, CategoryListing, Category, and Listing

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
+from .address_model import Address
+from .category_listing_model import CategoryListing
+from .category_model import Category
+from .listing_model import Listing
 from .product_model import Product
 from .user_model import User
 from .user_review_model import UserReview
