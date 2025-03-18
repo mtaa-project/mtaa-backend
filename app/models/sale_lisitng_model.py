90ea0a3180ee7b626b215aabf6d228528c3ba130
samuel-racak <samuel.racak@gmail.com>
2025-03-18 22:21:23 +0100

    feat: Add RentListing and SaleListing models to manage rental and sale transactions

---
+++ app/models/sale_lisitng_model.py
@@ -0,0 +1,13 @@
+from datetime import UTC, datetime
+
+from sqlmodel import Field, SQLModel
+
+
+class SaleListing(SQLModel, table=True):
+    __tablename__ = "saleListings"
+
+    listing_id: int = Field(foreign_key="listings.id", primary_key=True)
+    buyer_id: int = Field(foreign_key="users.id", primary_key=True)
+    sold_date: datetime = Field(
+        default_factory=lambda: datetime.now(UTC)
+    )  # TODO: check with team -> date when the listing was sold
