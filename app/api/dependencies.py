6e9d9b7203f97a01288f36d29b18f3b2f9628315
samuel-racak <samuel.racak@gmail.com>
2025-03-31 22:46:07 +0200

    refactor: Remove redundant user authentication check from request handling

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -15,6 +15,7 @@ async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
 
 
 async def get_user(request: Request):
-    if not hasattr(request.state, "user"):
-        raise HTTPException(status_code=401, detail="User not authenticated.")
+    # this is done by the middleware
+    # if not hasattr(request.state, "user"):
+    #     raise HTTPException(status_code=401, detail="User not authenticated.")
     return request.state.user
