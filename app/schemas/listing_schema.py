4c98c3b5d2ce4db9705ecff6ba32ccd590139ca7
samuel-racak <samuel.racak@gmail.com>
2025-03-31 22:45:44 +0200

    feat: Update Listing schemas to include Address and Category models for enhanced data representation

--- app/schemas/listing_schema.py
+++ app/schemas/listing_schema.py
@@ -1,7 +1,10 @@
 from decimal import Decimal
+from typing import List
 
 from sqlmodel import SQLModel
 
+from app.models.address_model import Address
+from app.models.category_model import Category
 from app.models.enums.listing_status import ListingStatus
 from app.models.enums.offer_type import OfferType
 
@@ -20,17 +23,16 @@ class ListingBase(SQLModel):
 class ListingCreate(ListingBase):
     address_id: int  # address visibility is handled in the address model
     category_ids: list[int]  # list of category ids
-    visibility: bool  # address visibility is used to set address visibility in address model  # if true, address is visible to all users
 
 
 # schema for listing view
 # this is used to read listing data from the database
 class ListingView(ListingBase):
-    address_id: int
-    category_ids: list[int]  # list of category ids
-    visibility: bool
+    address: Address
+    categories: list[Category]  # list of category ids
 
 
+# schema for listing update
 class ListingUpdate(SQLModel):
     title: str | None = None
     description: str | None = None
@@ -40,3 +42,14 @@ class ListingUpdate(SQLModel):
     address_id: int | None = None
     category_ids: list[int] | None = None
     visibility: bool | None = None
+
+
+class getParameters(SQLModel):
+    limit: int = 10
+    offset: int = 0
+    listing_status: ListingStatus | None = None
+    offer_type: OfferType | None = None
+    visibility: bool | None = None
+    category_ids: List[int] | None = None
+    min_price: int | None = None
+    max_price: int | None = None
