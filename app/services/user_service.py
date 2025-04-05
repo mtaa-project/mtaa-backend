f4b580e0450749c3d975c54b45be2fcde3e3ed06
SVBSH <getenv@protonmail.com>
2025-04-05 17:38:06 +0200

    feat: Implement UserService for user retrieval and authentication handling

---
+++ app/services/user_service.py
@@ -0,0 +1,65 @@
+from typing import List, Optional
+
+from fastapi import Depends, HTTPException, Request, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
+from sqlmodel import select
+
+from app.api.dependencies import get_async_session
+from app.models.user_model import User
+
+
+class UserService:
+    def __init__(self, session: AsyncSession, request: Request) -> None:
+        self.session = session
+        self.request = request
+
+        # getattr get attribute from class
+        # if attribute does not exist it returns second parameter (None)
+        self.user_metadata = getattr(request.state, "user", None)
+        if not self.user_metadata:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not authenticated.",
+            )
+
+    async def get_user_by_email(
+        self, email: Optional[str] = None, dependencies: Optional[List[str]] = None
+    ) -> User:
+        # returns email or None
+        email = email or self.user_metadata.get("email")
+        if not email:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User email not found in metadata.",
+            )
+
+        query = select(User).where(User.email == email)
+        dependencies = dependencies or []
+        if dependencies:
+            query = query.options(
+                *[selectinload(getattr(User, dep)) for dep in dependencies]
+            )
+        result = await self.session.execute(query)
+        db_user = result.scalars().one_or_none()
+        if not db_user:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="User not found in the database.",
+            )
+        return db_user
+
+    async def get_user(self, dependencies: Optional[List[str]] = None) -> User:
+        """
+        Retrieve the user using the email stored in the request state.
+        You can optionally provide a list of relationships to be preloaded.
+        """
+        return await self.get_user_by_email(dependencies=dependencies)
+
+    @classmethod
+    async def get_dependency(
+        cls,
+        request: Request,
+        session: AsyncSession = Depends(get_async_session),
+    ) -> "UserService":
+        return cls(session, request)
