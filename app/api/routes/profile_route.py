bbd5513dd5cb7048d335abc72b075b8bbabffdd5
SVBSH <getenv@protonmail.com>
2025-04-10 01:14:15 +0200

    Refactor user model and update profile route handling
    This commit includes the following changes:
    - Refactored the User model to inherit from UserBase, consolidating user attributes.
    - Updated the profile route to utilize new request and response schemas for user profile updates.
    - Removed unnecessary print statements and cleaned up the code for better readability.
    - Enhanced address handling logic during profile updates to ensure proper database interactions.

--- app/api/routes/profile_route.py
+++ app/api/routes/profile_route.py
@@ -1,62 +1,44 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from phonenumbers import PhoneNumberFormat
 from pydantic import BaseModel, Field
 from pydantic_extra_types.phone_numbers import PhoneNumber, PhoneNumberValidator
 from sqlalchemy.ext.asyncio.session import AsyncSession
-from sqlalchemy.sql.dml import Update
-from app.api.dependencies import get_async_session
-from phonenumbers import PhoneNumberFormat
-from app.models import User
-from app.models.address_model import Address
-from app.services.user.user_service import UserService
-from pydantic_extra_types.phone_numbers import PhoneNumber
-from app.schemas.address_schema import AddressUpdate
-from sqlalchemy.orm import selectinload
 from sqlmodel import select
+
+from app.api.dependencies import get_async_session
+from app.models.address_model import Address
+from app.schemas.user_schema import (
+    UserProfileUpdateRequest,
+    UserProfileUpdateResponse,
+)
+from app.services.user.user_service import UserService
+
 router = APIRouter(tags=["Profile"])
 
 
 class UserMetadata(BaseModel):
     firstname: str | None = Field(default=None, min_length=1, max_length=255)
     lastname: str | None = Field(default=None, min_length=1, max_length=255)
-    phone: Annotated[PhoneNumber | None, PhoneNumberValidator(
-        default_region=None, number_format='E164', supported_regions=None
-    )]
+    phone: Annotated[
+        PhoneNumber | None,
+        PhoneNumberValidator(
+            default_region=None, number_format="E164", supported_regions=None
+        ),
+    ]
     address_visibility: bool | None
 
     model_config = {
-            "json_schema_extra": {
-                "example": {
-                    "firstname": "Alice",
-                    "lastname": "Smith",
-                    "phone": "+421912345678",
-                    "address_visibility": True
-                }
+        "json_schema_extra": {
+            "example": {
+                "firstname": "Alice",
+                "lastname": "Smith",
+                "phone": "+421912345678",
+                "address_visibility": True,
             }
         }
-
-class AddressMetadata(BaseModel):
-    country: str | None = Field(..., min_length=2, max_length=100)
-    city: str | None = Field(..., min_length=1, max_length=255)
-    zip_code: str | None = Field(..., min_length=1, max_length=255)
-    street: str | None = Field(..., min_length=1, max_length=255)
-
-    model_config = {
-            "json_schema_extra": {
-                "example": {
-                    "country": "Slovakia",
-                    "city": "Bratislava",
-                    "zip_code": "84104",
-                    "street": "Karloveská 64"
-                }
-            }
-        }
-
-
-class UserProfileUpdate(BaseModel):
-    user_metadata: UserMetadata
-    address_metadata: AddressUpdate
+    }
 
 
 @router.get("/profile")
@@ -68,18 +50,17 @@ async def get_profile(
     current_user = await user_service.get_current_user()
 
     query_get_address = select(Address).where(
-        Address.user_id == current_user.id,
-        Address.is_primary == True
+        Address.user_id == current_user.id, Address.is_primary == True
     )
     user_address = await session.execute(query_get_address)
 
     return user_address.scalars().one_or_none()
 
 
-@router.put("/")
+@router.put("/", response_model=UserProfileUpdateResponse)
 async def update_profile(
     *,
-    update_data: UserProfileUpdate,
+    update_data: UserProfileUpdateRequest,
     session: AsyncSession = Depends(get_async_session),
     user_service: UserService = Depends(UserService.get_dependency),
 ):
@@ -88,28 +69,27 @@ async def update_profile(
 
     current_user = await user_service.get_current_user()
 
+    async with session.begin():
+        user_address = await session.execute(
+            select(Address).where(
+                Address.user_id == current_user.id, Address.is_primary == True
+            )
+        )
+        db_address = user_address.scalars().one_or_none()
+        # create user address
+        if db_address is None:
+            address_data["is_primary"] = True
+            address_data["user_id"] = current_user.id
+            db_address = Address.model_validate(address_data)
+        # update user address
+        else:
+            db_address.sqlmodel_update(address_data)
 
-    query_get_address = select(Address).where(
-        Address.user_id == current_user.id,
-        Address.is_primary == True
-    )
+        db_user = current_user.sqlmodel_update(user_data)
+        session.add_all([db_address, db_user])
 
-    user_address = await session.execute(query_get_address)
-    db_address = user_address.scalars().one_or_none()
-
-    if db_address is None:
-        address_data["is_primary"] = True
-        address_data["user_id"] = current_user.id
-        db_address = Address.model_validate(address_data)
-    else:
-        db_address.sqlmodel_update(address_data)
-
-
-    db_user = current_user.sqlmodel_update(user_data)
-
-    session.add_all([db_address, db_user])
     await session.commit()
     await session.refresh(db_address)
     await session.refresh(db_user)
-
-    return address_data
+    print(f"userdata: {db_user.firstname}")
+    return UserProfileUpdateResponse(user_metadata=db_user, address_metadata=db_address)
--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,8 +1,29 @@
-from sqlmodel import SQLModel
+from pydantic import BaseModel, EmailStr
+from sqlmodel import Field, SQLModel
+
+from app.schemas.address_schema import AddressBase, AddressUpdate
 
 
 class UserBase(SQLModel):
-    pass
+    firstname: str = Field(min_length=1, max_length=255)
+    lastname: str = Field(min_length=1, max_length=255)
+    email: EmailStr = Field(unique=True, max_length=255)
+    phone_number: str | None = Field(default=None, max_length=255)
+
 
 class UserUpdate(SQLModel):
-    pass
+    firstname: str = Field(min_length=1, max_length=255)
+    lastname: str = Field(min_length=1, max_length=255)
+    phone_number: str | None = Field(default=None, max_length=255)
+
+    model_config = {"extra": "forbid"}
+
+
+class UserProfileUpdateRequest(BaseModel):
+    user_metadata: UserUpdate
+    address_metadata: AddressUpdate
+
+
+class UserProfileUpdateResponse(BaseModel):
+    user_metadata: UserBase
+    address_metadata: AddressBase
