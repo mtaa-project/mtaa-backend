3d2e2b84a8916e62bb1f3de7360caabd39e69525
samuel-racak <samuel.racak@gmail.com>
2025-04-14 00:10:22 +0200

    feat: Update datetime imports and fix created_at and last_notified_at fields in UserSearchAlert model

--- app/models/user_search_alert_model.py
+++ app/models/user_search_alert_model.py
@@ -1,7 +1,7 @@
-from datetime import UTC, datetime
+from datetime import UTC, datetime, timezone
 from typing import TYPE_CHECKING, Any, Dict
 
-from sqlalchemy import Column
+from sqlalchemy import TIMESTAMP, Column
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlmodel import Field, Relationship, SQLModel
 
@@ -15,9 +15,13 @@ class UserSearchAlert(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     is_active: bool = Field(default=True)
     product_filters: Dict[str, Any] = Field(sa_column=Column(JSONB, nullable=False))
-    created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
-    last_notified_at: datetime | None = Field(
-        default=None
+    created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(TIMESTAMP(timezone=True), nullable=False),
+    )
+    last_notified_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc),
+        sa_column=Column(TIMESTAMP(timezone=True), nullable=False),
     )  # Used to track the last time the user was notified about new listings that match their search alert.
 
     # Foreign keys
