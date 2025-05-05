b957f0d171e5b8d198b3413ffbdad4860e1663a3
samuel-racak <samuel.racak@gmail.com>
2025-05-05 09:59:29 +0200

    fix: Remove TODO comment for defining relationship between Category and Listing

--- app/models/category_model.py
+++ app/models/category_model.py
@@ -14,7 +14,6 @@ class Category(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(max_length=255)
     # description: str = Field(max_length=255) TODO: check with the team
-    # TODO: define relationship between Category and Listing (Many-to-Many)
     listings: list["Listing"] = Relationship(
         back_populates="categories", link_model=CategoryListing
     )
